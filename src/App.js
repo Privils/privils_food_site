8017dd349ca09fe162f504a3626218dca316523b
Priviledge <privilledgeblack8@gmail.com>
2024-03-17 17:47:10 +0200

    routing errors fix part 2

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Header/>}>
       <Route index element={<Home/>}/>
-      <Route path='search_Meal' element={<SearchResult/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path="recipes/:mealId" element={<Recipe/>} />
+      <Route path='/search_Meal' element={<SearchResult/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path="/recipes/:mealId" element={<Recipe/>} />
     </Route>
   )
 )
