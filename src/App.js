1db05f3d130e0d99538ef2459cd93fdb431083b1
Priviledge <privilledgeblack8@gmail.com>
2024-04-06 16:59:21 +0200

    added new route

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
     <Router>
       <div>
         <Header />
+        <Home />
         <Routes>
         <Route index element={<Home/>}/>
       <Route path='/search_Meal' element={<SearchResult/>}/>
@@ -33,31 +34,3 @@ function App() {
 export default App;
 
 
-/*import './App.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import SearchResult from './components/SearchResult';
-import About from './components/About';
-import Recipe from './components/Recipe';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Header/>}>
-      <Route index element={<Home/>}/>
-      <Route path='/search_Meal' element={<SearchResult/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path="/recipes/:mealId" element={<Recipe/>} />
-    </Route>
-  )
-)
-function App() {
-  return (
-  <>
-  <RouterProvider router={router}/>
-  </>
-  );
-}
-
-export default App;
-*/
